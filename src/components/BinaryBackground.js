ab8cd682d047d2aa8556a6a55dc3ddc5d6dff166
xyber-099 <admin@xyberframe.com>
2024-07-23 21:02:28 +0500

    Update BinaryBackground.js

--- src/components/BinaryBackground.js
+++ src/components/BinaryBackground.js
@@ -4,7 +4,7 @@ import '../styles.css'; // Import your styles here
 const BinaryBackground = () => {
   useEffect(() => {
     const binaryContainer = document.querySelector('.binary-container');
-    const numBinaryItems = 4000; // Adjust the number of binary items as needed
+    const numBinaryItems = 3000; // Adjust the number of binary items as needed
 
     for (let i = 0; i < numBinaryItems; i++) {
       const binary = document.createElement('div');
